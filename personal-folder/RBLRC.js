5cf4ed285fd571b80c7429ce95b4bbab2e0a6019
adampattberg <pat23012@byui.edu>
2026-07-18 18:57:22 -0500

    javascript changes

---
+++ personal-folder/RBLRC.js
@@ -0,0 +1,71 @@
+const button = document.querySelector("#menu-button");
+const nav = document.querySelector("#main-nav");
+
+if (button && nav) {
+    button.addEventListener("click", toggleMenu);
+}
+
+function toggleMenu() {
+    nav.classList.toggle("open");
+}
+
+const image = document.querySelector("#car-image");
+
+if (image) {
+
+    let images = [];
+
+    if (window.location.pathname.includes("FZD2")) {
+
+        images = [
+            "styles/FZD2-1.jpg",
+            "styles/FZD2-2.jpg",
+            "styles/FZD2-3.jpg"
+        ];
+
+    }
+    else if (window.location.pathname.includes("Slash")) {
+
+        images = [
+            "styles/Slash-1.webp",
+            "styles/Slash-2.webp",
+            "styles/Slash-3.webp"
+        ];
+
+    }
+
+    let currentImage = 0;
+
+    function changeImage() {
+
+        currentImage++;
+
+        if (currentImage >= images.length) {
+            currentImage = 0;
+        }
+
+        image.src = images[currentImage];
+
+    }
+
+    setInterval(changeImage, 5000);
+
+}
+
+const searchButton = document.querySelector("#search-button");
+
+if (searchButton) {
+    searchButton.addEventListener("click", searchGoogle);
+}
+
+function searchGoogle() {
+
+    const searchTerm = searchButton.dataset.search;
+
+    window.open(
+        "https://www.google.com/search?q=" +
+        encodeURIComponent(searchTerm),
+        "_blank"
+    );
+
+}
